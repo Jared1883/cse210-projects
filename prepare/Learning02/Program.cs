f998aec0d4f8361d99a7bc074bf177cf3002990d
Jared1883 <jaredgudmundson@gmail.com>
2026-05-20 18:58:49 -0600

    finished prepare learning 02!!

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job = new Job();
+        job._company = "Microsoft";
+        job.Display();
+        Resume resume = new Resume();
+        resume._personName = "Juan Cruz";
+        resume._jobs.Add(job);
+        resume.DisplayJobs();
+
+
     }
 }
